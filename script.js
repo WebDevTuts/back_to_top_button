1218de31a2d5f775a9ee509faef2b816cd57b8ee
edwinchen85 <edwinchen85@gmail.com>
2017-05-15 20:25:29 +0800

    Define variables

---
+++ script.js
@@ -0,0 +1,6 @@
+var btt = document.getElementById('back-to-top'),
+    body = document.body,
+    docElem = document.documentElement,
+    offset = 100,
+    isFireFox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1,
+    scrollPos, docHeight;
