95a2fa372d5e985232cc86d2c24e1095e3f192ba
edwinchen85 <edwinchen85@gmail.com>
2017-05-15 20:27:41 +0800

    Calculate the document height

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ var btt = document.getElementById('back-to-top'),
     offset = 100,
     isFireFox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1,
     scrollPos, docHeight;
+
+// Calculate the document height
+docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docELem.offsetHeight);
+if (docHeight != 'undefined') {
+  offset = docHeight / 4;
+}
